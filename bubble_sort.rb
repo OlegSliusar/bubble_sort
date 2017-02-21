83edf5a1520943d98b423e4459eee932b2ca3ba6
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-21 13:11:04 +0200

    Define bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,17 @@
 def bubble_sort(array)
   (array.length - 1).times do
     (array.length - 1).times do |index|
-      if array[index] > array[index + 1]
-        tmp = array[index]
-        array[index] = array[index + 1]
-        array[index + 1] = tmp
+      array[index], array[index + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
+    end
+  end
+  array
+end
+
+def bubble_sort_by(array)
+  (array.length - 1).times do
+    (array.length - 1).times do |index|
+      if yield(array[index], array[index + 1]) > 0
+        array[index], array[index + 1] = array[index + 1], array[index]
       end
     end
   end
@@ -13,3 +20,5 @@ end
 
 p bubble_sort([4,3,78,2,0,2])
 p bubble_sort([6, 5, 4, 3, 2, 1, 0])
+
+p bubble_sort_by(["hi","hello","hey", "hawdy"]) { |left,right| left.length - right.length }
